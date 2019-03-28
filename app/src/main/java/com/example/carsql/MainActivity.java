f78a3f9dd92cc6bcc5f6f1e0436eb6376cf13cc9
jinmu333 <jinmu333@qq.com>
2019-03-28 09:48:47 +0800

    reinit

---
+++ app/src/main/java/com/example/carsql/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.carsql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.os.StrictMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    MysqlTest sql = new MysqlTest();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.textView7);
+        text.setText("此处显示查询到的信息");
+        text.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+    public void click(View v){
+        TextView text = (TextView) findViewById(R.id.textView7);
+
+        EditText editText=(EditText)findViewById(R.id.editText3);
+        String inputText=editText.getText().toString();
+
+        EditText editText1=(EditText)findViewById(R.id.editText5);
+        String inputText1=editText1.getText().toString();
+
+        EditText editText2=(EditText)findViewById(R.id.editText6);
+        String inputText2=editText2.getText().toString();
+
+        EditText editText3=(EditText)findViewById(R.id.editText7);
+        String inputText3=editText3.getText().toString();
+
+        EditText editText4=(EditText)findViewById(R.id.editText8);
+        String inputText4=editText4.getText().toString();
+
+        EditText editText5=(EditText)findViewById(R.id.editText9);
+        String inputText5=editText5.getText().toString();
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+        String[] out = new String[1000];
+        int[] count = new int[10];
+        sql.select(out, count, inputText, inputText1, inputText2);
+        String s = "" ;
+        String l = "--------------------------------------------------------------------------------------------\n";
+        int j = count[0];
+        while (j>=0){
+            s = s + l + out[j];
+            j--;
+        }
+        text.setText(s);
+        text.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+    public void click1(View v){
+        TextView text = (TextView) findViewById(R.id.textView7);
+        text.setText("关键词查询");
+        text.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+    public void click2(View v){
+        EditText editText=(EditText)findViewById(R.id.editText3);
+        editText.setText("");
+        EditText editText1=(EditText)findViewById(R.id.editText5);
+        editText1.setText("");
+        EditText editText2=(EditText)findViewById(R.id.editText6);
+        editText2.setText("");
+        EditText editText3=(EditText)findViewById(R.id.editText7);
+        editText3.setText("");
+        EditText editText4=(EditText)findViewById(R.id.editText8);
+        editText4.setText("");
+        EditText editText5=(EditText)findViewById(R.id.editText9);
+        editText5.setText("");
+
+        TextView text = (TextView) findViewById(R.id.textView7);
+        text.setText("此处显示查询到的信息");
+        text.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+}
---
+++ app/src/main/java/com/example/carsql/MysqlTest.java
@@ -0,0 +1,79 @@
+package com.example.carsql;
+
+import java.sql.*;
+
+public class MysqlTest {
+
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    public static void select(String[] out, int[] count, String DB_URL, String USER, String PASS) {
+        Connection conn = null;
+        Statement stmt = null;
+
+        DB_URL = "jdbc:mysql://"+"192.168.2.100"+":3306/"+"chepai";
+
+        // 数据库的用户名与密码，需要根据自己的设置
+        USER = "python";
+        PASS = "Python12345@";
+        try{
+            // 注册 JDBC 驱动
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM CARINFO";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //String[] out = new String[1000];
+            int i = 0;
+
+            // 展开结果集数据库
+            while(rs.next()){
+                System.out.println(rs);
+                // 通过字段检索
+                String TIME  = rs.getString("TIME");
+                String COLOR1 = rs.getString("COLOR1");
+                String TEXT1 = rs.getString("TEXT1");
+                String TEXT2 = rs.getString("TEXT2");
+                String API = rs.getString("API");
+                String SOURCE = rs.getString("SOURCE");
+                String output = " " + TIME + "\n " + COLOR1 + " " + TEXT1 +
+                        " " + TEXT2 + " " + API + " \n" + SOURCE;
+                // 输出数据
+                out[i] = output + "\n";
+                count[0] = i;
+                i++;
+            }
+            //System.out.println(out);
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
