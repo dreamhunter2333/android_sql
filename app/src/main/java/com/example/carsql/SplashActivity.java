9aa9d7298e298689e11897e3315d38563007c2e9
jinmu333 <jinmu333@qq.com>
2019-03-29 21:46:00 +0800

    启动界面

---
+++ app/src/main/java/com/example/carsql/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.carsql;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 2000; // 两秒后进入系统
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+}
